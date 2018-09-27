a6f9adb2d2f927d7c90c236e24aa1a193bb552ad
Simon Bojesen <simon@schonberg.dk>
2018-09-27 12:21:52 +0200

    login and register jsp's and cases

--- src/main/java/presentation/Control.java
+++ src/main/java/presentation/Control.java
@@ -41,41 +41,48 @@ public class Control extends HttpServlet {
             if (origin != null) {
                 switch (origin) {
                     case "login":
-                        String usernameLogin = request.getParameter("usernameLogin");
-                        String passwordLogin = request.getParameter("passwordLogin");
-                        User user = dm.authenticateLogin(usernameLogin, passwordLogin);
-                        
-                        if (user != null) {
-                            request.getSession().setAttribute("user", user);
-                            System.out.println(request.getSession().getAttribute(user.getUsername()));
-                            response.sendRedirect("Home");
-                        } else {
-                            request.setAttribute("error", "Unknown user, please try again");
-                            System.out.println(request.getAttribute("error"));
-                            request.getRequestDispatcher("login_and_register.jsp").forward(request, response);
-                        }
+                        caseLogin(request, dm, response);
                         break;
                     case "register":
-                        String usernameReg = request.getParameter("usernameRegister");
-                        String passwordReg = request.getParameter("passwordRegister");
-                        String emailReg = request.getParameter("email");
-                        boolean createUserResult = dm.createUser(usernameReg, passwordReg, emailReg);
-
-                        if (createUserResult == true) {
-                            request.setAttribute("CreateSucced", "Your account has been made");
-                            System.out.println(request.getAttribute("CreateSucced"));
-                            response.sendRedirect("login_and_register");
-                        } else {
-                            request.setAttribute("error", "Something went wrong, please try again");
-                            System.out.println(request.getAttribute("error"));
-                            response.sendRedirect("login_and_register");
-                        }
+                        caseRegister(request, dm, response);
                         break;
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Check stacktrace error");
+        }
+    }
+
+    private void caseRegister(HttpServletRequest request, DataMapper dm, HttpServletResponse response) throws IOException {
+        String usernameReg = request.getParameter("usernameRegister");
+        String passwordReg = request.getParameter("passwordRegister");
+        String emailReg = request.getParameter("email");
+        boolean createUserResult = dm.createUser(usernameReg, passwordReg, emailReg);
+        
+        if (createUserResult == true) {
+            request.setAttribute("CreateSucced", "Your account has been made");
+            System.out.println(request.getAttribute("CreateSucced"));
+            response.sendRedirect("login_and_register.html");
+        } else {
+            request.setAttribute("error", "Something went wrong, please try again");
+            System.out.println(request.getAttribute("error"));
+            response.sendRedirect("login_and_register.html");
+        }
+    }
+
+    private void caseLogin(HttpServletRequest request, DataMapper dm, HttpServletResponse response) throws IOException, ServletException {
+        String usernameLogin = request.getParameter("usernameLogin");
+        String passwordLogin = request.getParameter("passwordLogin");
+        User user = dm.authenticateLogin(usernameLogin, passwordLogin);
+        
+        if (user != null) {
+            request.getSession().setAttribute("user", user);
+            request.getRequestDispatcher("cupcakehome.jsp").forward(request, response);
+        } else {
+            request.setAttribute("error", "Unknown user, please try again");
+            System.out.println(request.getAttribute("error"));
+            request.getRequestDispatcher("login_and_register.html").forward(request, response);
         }
     }
 
